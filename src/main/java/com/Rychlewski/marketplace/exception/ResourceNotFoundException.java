55baf91a7224ca80c43edc319c853fdafa9d4706
KevinRychlewski <kevinsipioni13@gmail.com>
2026-03-04 01:05:22 -0300

    feat: implement global exception handling and validation handling

---
+++ src/main/java/com/Rychlewski/marketplace/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.Rychlewski.marketplace.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
